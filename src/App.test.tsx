4f67c96f2d2ffc2b085cb231219d69d93a5fa83b
Jean-baptiste Balmont <jbbalmont@o2commerce.com>
2024-11-12 16:06:51 -0500

    Feat: Update basic app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,24 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./components/footer/Footer", () => ({
+  Footer: () => <div data-testid="footer">Footer</div>,
+}));
+
+jest.mock("./components/navbar/NavBar", () => ({
+  NavBar: () => <div data-testid="navbar">NavBar</div>,
+}));
+
+jest.mock("./pages/RacesPage", () => ({
+  RacesPage: () => <div data-testid="races-page">RacesPage</div>,
+}));
+
+describe("Render App Component", () => {
+  test("renders NavBar, RacesPage, and Footer", () => {
+    render(<App />);
+
+    expect(screen.getByTestId("navbar")).toBeInTheDocument();
+    expect(screen.getByTestId("races-page")).toBeInTheDocument();
+    expect(screen.getByTestId("footer")).toBeInTheDocument();
+  });
 });
