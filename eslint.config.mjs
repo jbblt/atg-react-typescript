a56e3b5b12794267761602ab0e4c7ac0f51a897d
Jean-baptiste Balmont <jbbalmont@o2commerce.com>
2024-11-12 17:06:14 -0500

    Task: Eject CRA

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import perfectionist from 'eslint-plugin-perfectionist'
+import perfectionist from "eslint-plugin-perfectionist";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -14,65 +14,71 @@ export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
     languageOptions: { globals: globals.browser },
-    settings: { react: { version: "detect" },
+    ignores: [
+      "src/**/.test.*",
+      "src/**/.spec.*",
+    ],
+    settings: {
+      react: { version: "detect" },
       perfectionist: {
-        type: 'line-length',
+        type: "line-length",
         partitionByComment: true,
-      },},
+      },
+    },
     plugins: {
       perfectionist,
     },
     rules: {
-      'perfectionist/sort-named-imports': [
-        'error',
+      "perfectionist/sort-named-imports": [
+        "error",
         {
-          type: 'alphabetical',
-          order: 'asc',
+          type: "alphabetical",
+          order: "asc",
           ignoreAlias: false,
           ignoreCase: true,
-          specialCharacters: 'keep',
-          groupKind: 'mixed',
+          specialCharacters: "keep",
+          groupKind: "mixed",
           partitionByNewLine: false,
           partitionByComment: false,
-          matcher: 'minimatch',
+          matcher: "minimatch",
         },
       ],
-      'perfectionist/sort-imports': [
-        'error',
+      "perfectionist/sort-imports": [
+        "error",
         {
-          type: 'alphabetical',
-          order: 'asc',
+          type: "alphabetical",
+          order: "asc",
         },
       ],
       // "react-hooks/rules-of-hooks": "error",
       // "react-hooks/exhaustive-deps": "warn",
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
-      'no-useless-escape': 'off',
+      "no-useless-escape": "off",
       // Var is outdated use let/const instead for import
-      'no-var': 'warn',
-      'hydrogen/prefer-image-component': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/naming-convention': 'off',
-      '@typescript-eslint/no-empty-function': 'warn',
-      '@typescript-eslint/no-use-before-define': 'warn',
-      '@typescript-eslint/no-unused-expressions': 'warn',
-      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
-      'no-case-declarations': 'off',
-      'no-use-before-define': 'off',
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      "no-var": "warn",
+      "hydrogen/prefer-image-component": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/naming-convention": "off",
+      "@typescript-eslint/no-empty-function": "warn",
+      "@typescript-eslint/no-use-before-define": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "no-case-declarations": "off",
+      "no-use-before-define": "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
       // avoid using var module = require("module-x") instead => Es2015+ import
-      '@typescript-eslint/no-var-requires': ['error'],
+      "@typescript-eslint/no-var-requires": ["error"],
       // no any variable in case of typescript project
-      '@typescript-eslint/no-explicit-any': ['warn'],
+      "@typescript-eslint/no-explicit-any": ["warn"],
       // error on variable not used
-      '@typescript-eslint/no-unused-vars': 'error',
+      "@typescript-eslint/no-unused-vars": "error",
       // Use `` when dynamic string
-      'prefer-template': 'error',
+      "prefer-template": "error",
       // no ommit second argument in parseInt method (radix)
-      radix: 'error',
+      radix: "error",
       // send warn if formating no match prettier config
-      'prettier/prettier': ['error'],
+      "prettier/prettier": ["error"],
     },
   },
 ];
